34b00b973a0947d78492de3ef68b98a02e8cbc4d
harinsd404 <harinsd404@gmail.comgit config --global user.email harinsd404@gmail.com>
2025-08-03 14:22:13 +0900

    style: Header css 스타일 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,14 @@
 import { Link } from 'react-router-dom';
+import "../style/components.css";
 
 export default function Header(){
     return (
         <>
-            <header>
+            <header className='headerBox'>
                 <h1>Profile Card List</h1>
+                
             </header>
-            <nav>
+            <nav className='navBar'>
                 <Link to="/">Home</Link>
                 <Link to="">Card List</Link>
                 <Link to="">Make Card</Link>
