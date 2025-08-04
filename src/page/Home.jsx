9da57ccb270c7b8ec4a13c31f9770a55b394d71d
harinsd404 <harinsd404@gmail.com>
2025-08-04 23:50:57 +0900

    feat: textData값 렌더링

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,9 +1,19 @@
 import Header from '../components/Header';
 
 export default function Home(){
+    const textLine = [
+        "Home : 홈화면을 볼 수 있다.",
+        "CardList : 카드 리스트를 볼 수 있다.",
+        "Make Card : 카드를 만들 수 있다."
+    ]
+    const listItme = textLine.map(text=><li>{text}</li>);
+
     return(
         <>
             <Header />
+            <div>
+                <ul>{listItme}</ul>
+            </div>
         </>
     )
 }
