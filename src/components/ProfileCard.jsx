d8737cc16db6a5039720675fd549c430a78d4421
harinsd404 <harinsd168@gmail.com>
2025-08-08 09:16:54 +0900

    feat: ProfileCard UI 구성

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,1 +1,24 @@
-export default function ProfileCard() {}
+export default function ProfileCard({ data }) {
+    const image = "../assets/PARADOX_" + data.image;
+    let infoText = data.map(text=><p key={text}>{data.text}</p>);
+    infoText.pop();
+    infoText.pop(0);
+    
+    return (
+        <>
+            <div>
+                <div className="profileBox">
+                    <img src={image} alt="" />
+                    <h3>{profile.name}</h3>
+                </div>
+                <div className="profileInfo">
+                    <div className="textArea">{infoText}</div>
+                    <div className="btnArea">
+                        <button>수정</button>
+                        <button>삭제</button>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
